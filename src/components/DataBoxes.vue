3296496b338167ae8d317f7309d33a9fe3354e51
Michael Lajlev <lajlev@gmail.com>
2021-05-27 16:02:34 +0200

    done

---
+++ src/components/DataBoxes.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="grid md:grid-cols-2 gap-4">
+    <div class="shadow-md bg-blue-100 p-10 text-center rounded">
+      <h3 class="text-3xl text-blue-900 font-bold mb-4">
+        <i class="fas fa-head-side-cough"></i>
+        Cases
+      </h3>
+      <div class="text-2xl mb-4">
+        <span class="font-bold">New:</span>
+        {{ numberWithDots(stats.NewConfirmed)}}
+      </div>
+      <div class="text-2xl mb-4">
+        <span class="font-bold">Total:</span>
+        {{numberWithDots(stats.TotalConfirmed)}}
+      </div>
+    </div>
+    <div class="shadow-md bg-red-200 p-10 text-center rounded">
+      <h3 class="text-3xl text-black-900 font-bold mb-4">
+        <i class="fas fa-skull"></i>
+        Deaths
+      </h3>
+      <div class="text-2xl mb-4">
+        <span class="font-bold">New:</span>
+        {{numberWithDots(stats.NewDeaths)}}
+      </div>
+      <div class="text-2xl mb-4">
+        <span class="font-bold">Total:</span>
+        {{numberWithDots(stats.TotalDeaths)}}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DataBoxes',
+  props: ['stats'],
+  methods: {
+    numberWithDots(x) {
+      return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.')
+    }
+  }
+}
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <header class="text-center text-white p-4 mb-10 bg-blue-800">
+    <div class="text-3xl md:text-5xl font-bold mb-3">
+      <i class="fa fa-viruses"></i>
+      Covid-19 tracker
+    </div>
+    <p>
+      <a href="https://covid19api.com" target="_blank" class="text-blue-300">covid19api.com</a>
+    </p>
+  </header>
+</template>
+
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
